2abaf969b76cc8602beb7a3e946abe2741c5d416
Ahmed Ikram <ikramahmed398@gmail.com>
2025-12-28 22:48:45 +0500

    feat: updated User and added TenantUser entity

--- src/main/kotlin/com/example/ledgerone/entity/TenantUser.kt
+++ src/main/kotlin/com/example/ledgerone/entity/TenantUser.kt
@@ -1,3 +1,40 @@
 package com.example.ledgerone.entity
 
-data class TenantUser()
+import com.example.ledgerone.enum.TenantRole
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
+
+@Entity
+@Table(
+    name = "tenant_users",
+    uniqueConstraints = [
+        UniqueConstraint(columnNames = ["tenant_id", "user_id"])
+    ]
+)
+data class TenantUser(
+    @Id
+    @GeneratedValue(GenerationType.IDENTITY)
+    val id: Long,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tenant_id", nullable = false)
+    val tenant: Tenant,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    val tenantRole: TenantRole,
+)
