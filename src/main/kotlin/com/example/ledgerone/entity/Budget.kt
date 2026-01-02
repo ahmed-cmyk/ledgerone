9777cfe610df69eebe59915abf6654632e7a22db
Ahmed Ikram <ikramahmed398@gmail.com>
2026-01-02 23:23:01 +0500

    feat(Budget): added budget entity

---
+++ src/main/kotlin/com/example/ledgerone/entity/Budget.kt
@@ -0,0 +1,40 @@
+package com.example.ledgerone.entity
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
+import java.time.Period
+import java.time.ZonedDateTime
+
+@Entity
+@Table(name = "budget")
+class Budget (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
+
+    @OneToOne(cascade = [(CascadeType.REMOVE)], fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    val category: Category,
+
+    @Column(name = "monthly_limit", nullable = false)
+    val monthlyLimit: Double,
+
+    @Column(name = "period_start", nullable = false)
+    val periodStart: ZonedDateTime,
+
+    @Column(name = "period_end", nullable = false)
+    val periodEnd: ZonedDateTime,
+)
