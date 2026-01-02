d959eb57ab0017607b93c192b4b4c236eced3672
Ahmed Ikram <ikramahmed398@gmail.com>
2026-01-03 00:13:51 +0500

    feat(Notification): added Notification entity

---
+++ src/main/kotlin/com/example/ledgerone/entity/Notification.kt
@@ -0,0 +1,33 @@
+package com.example.ledgerone.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "notifications")
+class Notification (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
+
+    @Column(nullable = false)
+    val message: String,
+
+    @Column(nullable = false)
+    val createdAt: LocalDateTime,
+
+    @Column(nullable = false)
+    val read: Boolean = false,
+)
