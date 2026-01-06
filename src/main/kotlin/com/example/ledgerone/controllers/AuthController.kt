d0e28070be37f4cfe9f80f7b9c33f901211c1572
Ahmed Ikram <ikramahmed398@gmail.com>
2026-01-06 08:13:36 +0500

    refactor(AuthController): updated login method, added RequestMapping and renamed controller

--- src/main/kotlin/com/example/ledgerone/controllers/AuthController.kt
+++ src/main/kotlin/com/example/ledgerone/controllers/AuthController.kt
@@ -1,29 +1,33 @@
 package com.example.ledgerone.controllers
 
-import com.example.ledgerone.security.auth.UserPrincipal
 import org.springframework.http.ResponseEntity
-import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-class LoginController(
+@RequestMapping("/auth")
+class AuthController(
     private val authenticationManager: AuthenticationManager,
 ) {
 
     @PostMapping("/login")
     fun login(@RequestBody loginRequest: LoginRequest): ResponseEntity<Void> {
         val authenticationRequest = UsernamePasswordAuthenticationToken.unauthenticated(
-            loginRequest.username
-            , loginRequest.password
+            loginRequest.username,
+            loginRequest.password
         )
-        val authenticationResponse = authenticationManager.authenticate(authenticationRequest)
-        val principal = authenticationResponse.principal as UserPrincipal
-        return ResponseEntity.ok().build<Void>()
+
+        val authentication = authenticationManager.authenticate(authenticationRequest)
+
+        SecurityContextHolder.getContext().authentication = authentication
+
+        return ResponseEntity.ok().build()
     }
 
     data class LoginRequest(val username: String, val password: String)
